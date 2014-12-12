a8c20d018af93626992dc33e25b82aa6c513c805
Paul Kalauner <pkalauner@student.tgm.ac.at>
2014-12-12 13:12:20 +0100

    added Balancer

--- balancer/src/main/java/at/hollanderkalauner/picalc/balancer/Balancer.java
+++ balancer/src/main/java/at/hollanderkalauner/picalc/balancer/Balancer.java
@@ -1,11 +1,20 @@
 package at.hollanderkalauner.picalc.balancer;
 
+import at.hollanderkalauner.picalc.core.Calculator;
+
+import java.math.BigDecimal;
+import java.rmi.RemoteException;
+
 /**
  * Created by rene on 12/6/14.
  */
-public class Balancer {
+public class Balancer implements Calculator{
 
     public Balancer() {
     }
 
+    @Override
+    public BigDecimal pi(int decimalPlaces) throws RemoteException {
+        return null;
+    }
 }
