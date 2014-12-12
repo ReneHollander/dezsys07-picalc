dca175150d7fd9dd75d275da1560acbe2962afc7
ReneHollander <rene.hollander@hotmail.de>
2014-12-12 19:17:36 +0100

    fixed missing bind in calculator Main

--- picalc-calculator/src/main/java/at/hollanderkalauner/picalc/calculator/Main.java
+++ picalc-calculator/src/main/java/at/hollanderkalauner/picalc/calculator/Main.java
@@ -1,26 +1,14 @@
 package at.hollanderkalauner.picalc.calculator;
 
-import at.hollanderkalauner.picalc.core.calculationbehaviour.GaussLegendre;
-import at.hollanderkalauner.picalc.core.Calculator;
-import at.hollanderkalauner.picalc.core.CalculatorRegistry;
-import at.hollanderkalauner.picalc.core.RMIUtil;
-import at.hollanderkalauner.picalc.core.Static;
-
-import java.net.MalformedURLException;
-import java.rmi.AlreadyBoundException;
-import java.rmi.Naming;
-import java.rmi.NotBoundException;
-import java.rmi.RemoteException;
-
 /**
  * Created by rene on 12/6/14.
  */
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        Calculator calculator = new CalculatorService(CalculatorService.Mode.BEHINDBALANCER);
+        CalculatorService calculatorService = new CalculatorService(CalculatorService.Mode.BEHINDBALANCER);
+        calculatorService.bind(CalculatorService.Mode.BEHINDBALANCER);
     }
-
 
 
 }
