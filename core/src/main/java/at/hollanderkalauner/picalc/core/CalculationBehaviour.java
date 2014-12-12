d83a8f8f673e7b380ca7d077806191cd706e852e
ReneHollander <rene.hollander@hotmail.de>
2014-12-12 12:46:36 +0100

    added calculationbehaviour

---
+++ core/src/main/java/at/hollanderkalauner/picalc/core/CalculationBehaviour.java
@@ -0,0 +1,12 @@
+package at.hollanderkalauner.picalc.core;
+
+import java.math.BigDecimal;
+
+/**
+ * Created by rene on 12/12/14.
+ */
+public interface CalculationBehaviour {
+
+    public BigDecimal calculatePi(int decimalPlaces);
+
+}
