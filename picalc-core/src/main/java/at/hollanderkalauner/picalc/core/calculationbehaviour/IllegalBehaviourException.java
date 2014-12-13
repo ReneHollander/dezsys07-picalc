1854355db35754d7bc018e204c0990f8a5cd4591
ReneHollander <rene.hollander@hotmail.de>
2014-12-13 11:16:50 +0100

    fixed broken javadoc

--- picalc-core/src/main/java/at/hollanderkalauner/picalc/core/calculationbehaviour/IllegalBehaviourException.java
+++ picalc-core/src/main/java/at/hollanderkalauner/picalc/core/calculationbehaviour/IllegalBehaviourException.java
@@ -29,10 +29,9 @@ public class IllegalBehaviourException extends IllegalArgumentException {
     /**
      * Constructs a new exception with the specified detail message and
      * cause.
-     * <p/>
      * <p>Note that the detail message associated with <code>cause</code> is
      * <i>not</i> automatically incorporated in this exception's detail
-     * message.
+     * message.</p>
      *
      * @param message the detail message (which is saved for later retrieval
      *                by the {@link Throwable#getMessage()} method).
