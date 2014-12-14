1b2c2613f99efd776b6d384cfd67043d173dc0ff
ReneHollander <rene.hollander@hotmail.de>
2014-12-14 10:10:44 +0100

    added integrationtest module

---
+++ picalc-integrationtest/src/test/java/at/hollanderkalauner/picalc/integrationtest/IntegrationTest.java
@@ -0,0 +1,10 @@
+package at.hollanderkalauner.picalc.integrationtest;
+
+/**
+ * Integration Tests for PiCalc
+ *
+ * @author Rene Hollander
+ * @version 20141214.1
+ */
+public class IntegrationTest {
+}
