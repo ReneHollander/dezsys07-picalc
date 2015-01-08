28fa59793f8f43cde6bddd9cbba5c1a8e6aee27a
ReneHollander <rene.hollander@hotmail.de>
2015-01-08 17:46:05 +0100

    added small deploy script to create a complete zip containing all modules

---
+++ deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+NAME=picalc_hollander_kalauner
+
+rm -Rf $NAME
+mkdir -p $NAME
+
+JARS=(picalc-balancer picalc-calculator picalc-client picalc-integrationtest)
+
+for i in ${JARS[@]}; do
+  cp ${i}/final/${i}.jar $NAME/
+done
+
+mkdir -p $NAME/doc
+find files/docs/* \( -name '*.svg' -o -name '*.html' -o -name '*.pdf' \) -exec cp {} $NAME/doc \;
+#cp files/docs/* $NAME/doc
+
+git log --stat > $NAME/repo.log
+
+zip -r $NAME.zip $NAME/*
